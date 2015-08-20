d08d1eb3f9677c9dedefae6bf0155201f6723016
Michael Richardson <michael@michaelandhelen.com>
2015-08-20 22:06:35 +1000

    url typo

--- files/start.sh
+++ files/start.sh
@@ -4,8 +4,7 @@ set -e
 # if $proxy_domain is not set, then default to $HOSTNAME
 export proxy_domain=${proxy_domain:-$HOSTNAME}
 
-# ensure the following environment variables are set. 
-# exit script and container if not set.
+# ensure the following environment variables are set. exit script and container if not set.
 test $backend_host
 test $load_balancer_domain
 test $app_servers
